687acdbabfd6bdc2bfca75980356097fead5b98e
josh <8550998+josh-Fen@users.noreply.github.com>
2024-05-07 21:24:22 -0400

    Add sketchpad via JS file

---
+++ script.js
@@ -0,0 +1,10 @@
+const container = document.querySelector('.container');
+
+for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+        let block = document.createElement('div');
+        block.classList.add('pixel');
+        container.appendChild(block);
+    }
+}
+
