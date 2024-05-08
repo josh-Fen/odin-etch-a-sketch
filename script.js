554b39f109200eec22874723a1d0868e1f5cadee
josh <8550998+josh-Fen@users.noreply.github.com>
2024-05-07 22:15:59 -0400

    Add functionality to resize resolution of sketchpad and refactor

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
+const btn = document.querySelector('button');
 const container = document.querySelector('.container');
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        let block = document.createElement('div');
-        block.classList.add('pixel');
-        container.appendChild(block);
+function draw(num) {
+    container.replaceChildren();
+    const newWidth = 640 / num;
+    for (let i = 0; i < num; i++) {
+        for (let j = 0; j < num; j++) {
+            let block = document.createElement('div');
+            block.classList.add('pixel');
+            block.style.width = newWidth + "px";
+            block.style.height = newWidth + "px";
+            container.appendChild(block);
+        }
     }
+    setUpBlocks();
 }
 
-const blocks = container.querySelectorAll('div');
-
-blocks.forEach((block) => {
-    block.addEventListener('mouseover', () => {
-        block.classList.add('color-change');
+function setUpBlocks() {
+    const blocks = container.querySelectorAll('div');
+    blocks.forEach((block) => {
+        block.addEventListener('mouseover', () => {
+            block.classList.add('color-change');
+        });
     });
+}
+
+btn.addEventListener('click', () => {
+    let num = +prompt('Please enter how many square you would like per row (max 100)');
+    while (num > 100) {
+        num = +prompt('Please enter how many square you would like per row (max 100)');
+    }
+    draw(num);
 });
+
+draw(16);
+setUpBlocks();
+
+
